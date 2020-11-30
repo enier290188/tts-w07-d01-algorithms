365492775c6cf415a056d5b7a855b07c8387cfe0
Enier Ramos García <enier290188@gmail.com>
2020-11-30 15:31:20 -0500

    Bubble Sort Assignment.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 - Write a program to sort an array of given integers using the Insertion Sort Algorithm.
 - Write a program to sort an array of given integers using the Merge Sort Algorithm.
 - Write a program to sort an array of given integers using the Quick Sort Algorithm.
+
+# TTS: Week 07, Day 01. Bubble Sort Assignment.
+Complete the assignment found at: [http://programmingbydoing.com/a/bubble-sort.html](http://programmingbydoing.com/a/bubble-sort.html "http://programmingbydoing.com/a/bubble-sort.html")
---
+++ src/com/tts/BubbleSort.java
@@ -0,0 +1,50 @@
+package com.tts;
+
+import java.util.Random;
+
+public class BubbleSort {
+    public static void bubble_sort(int[] a) {
+        // Your code goes here
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] a, int i, int j) {
+        // Your code goes here
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[] arr = new int[10];
+        int i;
+
+        // Fill up the array with random numbers
+        for (i = 0; i < arr.length; i++)
+            arr[i] = 1 + r.nextInt(100);
+
+        // Display it
+        System.out.print("before: ");
+        for (i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+
+        // Sort it
+        bubble_sort(arr);
+
+        // Display it again to confirm that it's sorted
+        System.out.print("after : ");
+        for (i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
